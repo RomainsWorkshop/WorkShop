04a8434cd62f22411bdbad72295f089a650aba7d
MarlonPMan <Theforgottendreamer@protonmail.com>
2024-06-16 12:54:25 -0400

    mdd

---
+++ Load.js
@@ -0,0 +1,8 @@
+var txtFile = new XMLHttpRequest();
+txtFile.open("GET", "https://romainsworkshop.github.io/WorkShop//Intro.txt", true);
+txtFile.onreadystatechange = function() {
+  if (txtFile.readyState === 4 && txtFile.status == 200) {
+     allText = txtFile.responseText;
+  }
+}
+document.getElementById('Jonha').innerHTML = allText;
